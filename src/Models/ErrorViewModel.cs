27739440422399eee693237bf6b9a3880221168c
James White <james@gbase.com>
2021-04-16 18:17:17 -0700

    Initial Project Created

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DealerVision.Components.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
